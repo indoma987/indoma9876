d5c72b87789ff5d33ae7f8a2514b5a88183536c2
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-03-08 09:25:25 -0300

    dfcvgr

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ var texto = document.querySelector('span');
 
 texto.textContent = nomeUsuario;
 
-while(nomeUsuario == '' || ){
+while(nomeUsuario == '' ) { // || nomeUsuario == null
     nomeUsuario = prompt('Qual seu nome??')
 }
 
+if (nomeUsuario == null){ 
+  texto.textContent = 'Usuario';
+} else { 
+    texto.textContent = 
+}
