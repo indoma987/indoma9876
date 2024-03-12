85e7dd54e68f3e665d906d471c9d6d6ec1a5f11e
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-03-12 08:11:26 -0300

    78i7878

--- jogo.js
+++ jogo.js
@@ -6,11 +6,10 @@ while(chute == ''){
     chute = prompt('Escolha um número entre 0 a 10.');
 }
 
-
 if(numeroSecreto == chute){
     alert('Parabéns!!!!! Você acertou :)');
     console.log('Parabéns, acertou krh');
 } else{
-    alert('Que pena, você não acertouu ');
+    alert('Que pena, você não acertouu :(');
     console.log('Infelizmente, errou krh :(');
 }
