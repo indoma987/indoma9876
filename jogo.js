24a60749b94a72614cf6f495ae1baf61d3d74b88
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-03-12 08:00:59 -0300

    v fytut

--- jogo.js
+++ jogo.js
@@ -1,1 +1,14 @@
-let numeroSecreto = 7;
+let numeroSecreto == 7;
+
+prompt('Escolha um número entre 0 a 10');
+
+
+
+
+if(numeroSecreto == chute){
+    alert('Parabéns!!!!! Você acertou :)');
+    console.log('Parabéns, acertou krh');
+} else{
+    alert('Que pena, você não acertouu ');
+    console.log('Infelizmente, errou krh :(');
+}
