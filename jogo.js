e33ab382b5198bf519319f771db6911813a2aa0e
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-03-12 08:28:31 -0300

    ert4e

--- jogo.js
+++ jogo.js
@@ -1,19 +1,14 @@
-let numeroSecreto = 7;
+let numeroSecreto =  Math.random() * 20;
+console.log(numeroSecreto);
 
 let chute = '';
 
-while(chute == ''){
-    chute = prompt('Escolha um número entre 0 a 10.');
-}
 
-<<<<<<< HEAD
 while(chute == null || chute != numeroSecreto){
     alert('tente novamente :)')
-    chute = prompt('escolha um numero entre 0 a 10.');
+    chute = prompt('escolha um numero entre 0 a 20.');
 }
 
-=======
->>>>>>> 85e7dd54e68f3e665d906d471c9d6d6ec1a5f11e
 if(numeroSecreto == chute){
     alert('Parabéns!!!!! Você acertou :)');
     console.log('Parabéns, acertou krh');
