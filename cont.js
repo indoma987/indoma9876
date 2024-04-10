e18c6304c995e589dcff54baccfd7972d01e01b7
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-04-10 10:23:59 -0300

    scsa

--- cont.js
+++ cont.js
@@ -1,14 +1,15 @@
 const botoes = document.querySelectorAll('.objetivos');
-console.log(botoes.length);
+const textos = document.querySelectorAll('.aba-conteudo');
 
 for (let sas = 0; sas < botoes.length; sas++){
 console.log(sas);
 botoes[sas].onclick = function(){
     for(let j = 0; j < botoes.length; j++){
         botoes[j].classList.remove('ativo');
+        textos[j].classList.remove('ativa');
     }
 botoes[sas].classList.add('ativo');
-
+texto[sas].classList.add('ativa');
 }
 }
 
