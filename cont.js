e697ec73a66a5c1c8bc7a56804553bd4d271b371
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-04-10 09:22:50 -0300

    jmj

--- cont.js
+++ cont.js
@@ -4,6 +4,9 @@ console.log(botoes.length);
 for (let sas = 0; sas < botoes.length; sas++){
 console.log(sas);
 botoes[sas].onclick = function(){
+    for(let j = 0; j < botoes.length; j++){
+        botoes[j].classList.remove('ativo');
+    }
 botoes[sas].classList.add('ativo');
 
 }
