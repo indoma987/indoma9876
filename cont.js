7417060b2164f90541099e3fedc9aebc6e6e254f
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-04-17 10:13:38 -0300

    ut6u

--- cont.js
+++ cont.js
@@ -21,6 +21,8 @@ let tempoFinal = tempoObjetivo1 - tempoAtual;
 
 let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 
 contadores[0].textContent = minutos;
 
