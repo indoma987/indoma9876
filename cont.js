2fd61b5292a5c45b6c3bb72311295f57a2a99c97
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-04-17 10:22:36 -0300

    nmhm

--- cont.js
+++ cont.js
@@ -23,5 +23,9 @@ let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
 let dias = Math.floor(horas/24);
+ 
+segundos % 60;
+minutos % 60;
+horas % 24;
 
 contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
