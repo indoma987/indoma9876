9952518b9360993ca570d195ddba089647e32153
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-04-17 09:32:52 -0300

    5y5ty

--- cont.js
+++ cont.js
@@ -15,8 +15,8 @@ textos[sas].classList.add('ativa');
 }
 
 const contadores = document.querySelectorAll('.contador');
-const tempoObjetivo1 = new Date ('2024-04-18T00:00:00');
+const tempoObjetivo1 = new Date ('2024-04-19T00:00:00');
 let tempoAtual = new Date();
 
-contadores[0].textContent = 'Contagem';
+contadores[0].textContent = tempoObjetivo - tempoAtual;
 
