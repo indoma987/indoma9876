4117779229717eccd7860142116ba7112fcfb595
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-05-10 09:05:14 -0300

    ku

--- cont.js
+++ cont.js
@@ -21,7 +21,7 @@ const tempoObjetivo3 = new Date ('2024-07-10T00:00:00');
 const tempoObjetivo4 = new Date ('2025-02-25T00:00:00');
 
 
-let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 for(let i = 0; i< contadores.length; i++){
 contadores[i].textContent = CalculaTempo(tempos[i]);
 }
